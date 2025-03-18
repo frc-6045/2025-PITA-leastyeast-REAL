d697b89194a4a10714edbd6c9098eca71488981b
allison zhang <ayusfun1@gmail.com>
2025-03-18 17:19:18 -0500

    Henry fix auto

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -100,7 +100,7 @@ public class Autos {
         autoChooser.addOption("Do Nothing", new InstantCommand(() -> {System.out.println("hi");}));
         autoChooser.addOption("test", AutoBuilder.buildAuto("New New Auto"));
         autoChooser.addOption("3PieceIKJPolesLimelight", AutoBuilder.buildAuto("3PieceIKJPolesLimelight"));
-        autoChooser.addOption("(Left)Coral2AlgaeHGHKJPoles", AutoBuilder.buildAuto("Coral2AlgaeHGHKJPoles"));
+        autoChooser.addOption("(Left)Coral2AlgaeHGHIJPoles", AutoBuilder.buildAuto("Coral2AlgaeHGHIJPoles"));
         autoChooser.addOption("do not run-(Right)Coral2AlgaeHGHEFPoles", AutoBuilder.buildAuto("Coral2AlgaeHGHEFPoles"));
         SmartDashboard.putData("autos", autoChooser);
     }
