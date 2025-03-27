82ca66edf23b40d0691d0ab47758adefcdb78421
allison zhang <ayusfun1@gmail.com>
2025-03-27 18:20:54 -0500

    Break autos

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -41,59 +41,56 @@ public class Autos {
             new AutoScoreCommands(m_SwerveSubsystem, m_Arm, m_Elevator, m_Intake);
 
         // Named Commands //
-        NamedCommands.registerCommand("coralSpinNormal", new IntakeClosedLoop(intake, 1, false)); //score for L3, L4, algae out
-        NamedCommands.registerCommand("coralSpinOther", new IntakeClosedLoop(intake, 0.5, true)); //score for L2, L1
-        NamedCommands.registerCommand("algaeInOne", new IntakeClosedLoop(intake, 1.2, false)); //algae in
-        NamedCommands.registerCommand("algaeInTwo", new IntakeClosedLoop(intake, 2.1, false)); //algae in
-        NamedCommands.registerCommand("coralIntake", new IntakeIntakeClosedLoop(intake, ()->{return intake.coralDetected();}, MotorConstants.kIntakeMotorSpeed));
-        NamedCommands.registerCommand("coralL1", new PIDArmAndElevator(arm, elev, Setpoints.L1).asProxy());
-        NamedCommands.registerCommand("coralL2", new PIDArmAndElevator(arm, elev, Setpoints.L2).asProxy());
-        NamedCommands.registerCommand("coralL3", new PIDArmAndElevator(arm, elev, Setpoints.L3).asProxy());
-        NamedCommands.registerCommand("coralL4", new PIDArmAndElevator(arm, elev, Setpoints.L4).asProxy());
-        NamedCommands.registerCommand("algaeHigh", new PIDArmAndElevator(arm, elev, Setpoints.ALGAE_HIGH).asProxy());
-        NamedCommands.registerCommand("algaeLow", new PIDArmAndElevator(arm, elev, Setpoints.ALGAE_LOW).asProxy());
-        NamedCommands.registerCommand("barge", new ParallelCommandGroup(
-                new PIDArmAndElevator(arm, elev, Setpoints.BARGE).asProxy(),
-                new IntakeConditional(intake, () -> {return arm.getSketchyOffsettedPosition()<0.6;}, true, 0.9)
-                ));
-        NamedCommands.registerCommand("bargeNew",
-            new ParallelCommandGroup(
-                new SequentialCommandGroup(
-                    Commands.none().until(() -> elev.getRelativeEncoderPosition()<-50),
-                    new PIDArmCommand(arm, PositionConstants.kBargeArm).asProxy()),
-                new PIDElevatorCommand(elev, PositionConstants.kBargeElev),
-                new IntakeConditional(intake, () -> {return arm.getSketchyOffsettedPosition()<0.6;}, true, 0.9)
-            ));
-        NamedCommands.registerCommand("coralIntakeSetpoint", new PIDArmAndElevator(arm, elev, Setpoints.INTAKE).asProxy());
-        NamedCommands.registerCommand("homePosition", new PIDArmAndElevator(arm, elev, Setpoints.HOME).asProxy());
-        NamedCommands.registerCommand("alignToReefScoreL3Left", 
-            m_AutoScoreCommands.scoreNearestReefFace(
-                Setpoints.L3,
-                ()->{return Side.LEFT;},
-                ()->{return intake.getAlignOffset();}
-            )
-        );
-        NamedCommands.registerCommand("alignToReefScoreL3Right", 
-            m_AutoScoreCommands.scoreNearestReefFace(
-                Setpoints.L3,
-                ()->{return Side.RIGHT;},
-                ()->{return intake.getAlignOffset();}
-            )
-        );
-        NamedCommands.registerCommand("alignToReefScoreL4Left", 
-            m_AutoScoreCommands.scoreNearestReefFace(
-                Setpoints.L4,
-                ()->{return Side.LEFT;},
-                ()->{return intake.getAlignOffset();}
-            )
-        );
-        NamedCommands.registerCommand("alignToReefScoreL4Right", 
-            m_AutoScoreCommands.scoreNearestReefFace(
-                Setpoints.L4,
-                ()->{return Side.RIGHT;},
-                ()->{return intake.getAlignOffset();}
-            )
-        );
+        NamedCommands.registerCommand("coralSpinNormal", new InstantCommand(() -> {})); //score for L3, L4, algae out
+        NamedCommands.registerCommand("coralSpinOther", new InstantCommand(() -> {})); //score for L2, L1
+        NamedCommands.registerCommand("algaeInOne", new InstantCommand(() -> {})); //algae in
+        NamedCommands.registerCommand("algaeInTwo", new InstantCommand(() -> {})); //algae in
+        NamedCommands.registerCommand("coralIntake", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("coralL1", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("coralL2", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("coralL3", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("coralL4", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("algaeHigh", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("algaeLow", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("barge", new InstantCommand(() -> {}));
+        // NamedCommands.registerCommand("bargeNew",
+        //     new ParallelCommandGroup(
+        //         new SequentialCommandGroup(
+        //             Commands.none().until(() -> elev.getRelativeEncoderPosition()<-50),
+        //             new PIDArmCommand(arm, PositionConstants.kBargeArm).asProxy()),
+        //         new PIDElevatorCommand(elev, PositionConstants.kBargeElev),
+        //         new IntakeConditional(intake, () -> {return arm.getSketchyOffsettedPosition()<0.6;}, true, 0.9)
+        //     ));
+        NamedCommands.registerCommand("coralIntakeSetpoint", new InstantCommand(() -> {}));
+        NamedCommands.registerCommand("homePosition", new InstantCommand(() -> {}));
+        // NamedCommands.registerCommand("alignToReefScoreL3Left", 
+        //     m_AutoScoreCommands.scoreNearestReefFace(
+        //         Setpoints.L3,
+        //         ()->{return Side.LEFT;},
+        //         ()->{return intake.getAlignOffset();}
+        //     )
+        // );
+        // NamedCommands.registerCommand("alignToReefScoreL3Right", 
+        //     m_AutoScoreCommands.scoreNearestReefFace(
+        //         Setpoints.L3,
+        //         ()->{return Side.RIGHT;},
+        //         ()->{return intake.getAlignOffset();}
+        //     )
+        // );
+        // NamedCommands.registerCommand("alignToReefScoreL4Left", 
+        //     m_AutoScoreCommands.scoreNearestReefFace(
+        //         Setpoints.L4,
+        //         ()->{return Side.LEFT;},
+        //         ()->{return intake.getAlignOffset();}
+        //     )
+        // );
+        // NamedCommands.registerCommand("alignToReefScoreL4Right", 
+        //     m_AutoScoreCommands.scoreNearestReefFace(
+        //         Setpoints.L4,
+        //         ()->{return Side.RIGHT;},
+        //         ()->{return intake.getAlignOffset();}
+        //     )
+        // );
 
         // Autos //
         autoChooser = new SendableChooser<Command>();
