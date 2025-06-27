6bcc4cd41743cd40da194912761e61cd485632ac
allison zhang <ayusfun1@gmail.com>
2025-06-27 18:25:53 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -151,7 +151,7 @@ public final class Constants {
     public static final double ROBOT_MASS = (149) * 0.453592; // lbs * kg per pound
     public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
     public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-    public static final double MAX_SPEED = Units.feetToMeters(14); // was 25
+    public static final double MAX_SPEED = Units.feetToMeters(5); // was 25
   }
 
   public static class AutoScoreConstants {
